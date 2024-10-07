c4eb09a42062081ac43127ce1bd72e93e974b442
mrandr1an <krackedissad@gmail.com>
2024-10-07 12:39:36 +0300

    feat(parser)!: add initial structure for parser combinators

--- src/frontend/parsers/parser.rs
+++ src/frontend/parsers/parser.rs
@@ -1,134 +1,106 @@
-use std::{error::Error, ops::Range};
+use std::{error::Error, fmt::Debug, ops::Range};
 
 use miette::{Diagnostic, NamedSource, SourceSpan};
 use thiserror::Error;
 
-use crate::frontend::lexers::{lexer::Tokens, token::Token};
+use crate::frontend::lexers::{lexer::Lexer, token::TokenKind};
 
-pub type ParserResult<'parser, Output, Rest, Err> = miette::Result<(Output, Rest), Err>;
-
-pub type Input<'parser, Language, Lexer> = Tokens<'parser, Language, Lexer>;
-
-pub trait Parser<
-    'parser,
-    Language,
-    Lexer: Iterator<Item = Token<'parser, Language>>,
-    Output,
-    Err: Error + Diagnostic,
->
-{
-    fn parse(
-        &self,
-        input: &'parser mut Input<'parser, Language, Lexer>,
-    ) -> ParserResult<'parser, Output, &'parser mut Input<'parser, Language, Lexer>, Err>;
-
-    fn map<F, MappedOutput>(
-        &self,
-        input: &'parser mut Input<'parser, Language, Lexer>,
-        map_fn: F,
-    ) -> ParserResult<'parser, MappedOutput, &'parser mut Input<'parser, Language, Lexer>, Err>
-    where
-        F: Fn(Output) -> MappedOutput,
-    {
-        self.parse(input)
-            .map(|(output, rest)| (map_fn(output), rest))
-    }
-}
-
-impl<
-        'parser,
-        F,
-        Language: 'parser,
-        Lexer: Iterator<Item = Token<'parser, Language>> + 'parser,
-        Output,
-        Err: Error + Diagnostic,
-    > Parser<'parser, Language, Lexer, Output, Err> for F
-where
-    F: Fn(
-        &'parser mut Input<'parser, Language, Lexer>,
-    ) -> ParserResult<'parser, Output, &'parser mut Input<'parser, Language, Lexer>, Err>,
-{
-    fn parse(
-        &self,
-        input: &'parser mut Input<'parser, Language, Lexer>,
-    ) -> ParserResult<'parser, Output, &'parser mut Input<'parser, Language, Lexer>, Err> {
-        self(input)
-    }
-}
-
-pub fn pair<
-    'parser,
-    P1,
-    P2,
-    L: 'parser,
-    Lexer: Iterator<Item = Token<'parser, L>> + 'parser,
-    O1,
-    O2,
-    E: Error + Diagnostic,
->(
-    p1: P1,
-    p2: P2,
-) -> impl Parser<'parser, L, Lexer, (O1, O2), E>
-where
-    P1: Parser<'parser, L, Lexer, O1, E>,
-    P2: Parser<'parser, L, Lexer, O2, E>,
-{
-    move |input| match p1.parse(input) {
-        Ok((output1, rest1)) => match p2.parse(rest1) {
-            Ok((output2, rest2)) => Ok(((output1, output2), rest2)),
-            Err(err) => Err(err),
-        },
-        Err(err) => Err(err),
-    }
-}
+pub type ParserR<'parser, Output, Error> =
+    miette::Result<(Output, &'parser mut Lexer<'parser>), Error>;
 
 #[derive(Error, Diagnostic, Debug)]
-#[error("Error parsing file")]
-pub struct UnexpectedEOF {
+pub enum Either<E1: Error + Diagnostic, E2: Error + Diagnostic> {
+    #[error(transparent)]
+    Left(E1),
+    #[error(transparent)]
+    Right(E2),
+}
+
+impl<E1: Error + Diagnostic, E2: Error + Diagnostic> Either<E1, E2> {}
+
+#[derive(Error, Diagnostic, Debug)]
+#[error("Parser Error")]
+pub struct Expected<O: std::fmt::Debug> {
+    expected: O,
     #[source_code]
     src: NamedSource<String>,
     #[label("Here")]
     bad_bit: SourceSpan,
 }
 
-impl UnexpectedEOF {
-    pub fn new(name: &str, src: &str, range: Range<usize>) -> Self {
-        UnexpectedEOF {
+impl<O: std::fmt::Debug> Expected<O> {
+    pub fn new(expected: O, name: &str, src: &str, range: Range<usize>) -> Self {
+        Self {
+            expected,
             src: NamedSource::new(name, src.to_string()),
             bad_bit: range.into(),
         }
     }
 }
 
-#[derive(Error, Diagnostic, Debug)]
-#[error("Error parsing value")]
-pub enum ValueError {
-    #[diagnostic(
-        code(Parser::Value::UnexpectedToken),
-        help("Enter a character instead")
-    )]
-    ExpectedChar(
-        #[source_code] NamedSource<String>,
-        #[label("Here")] SourceSpan,
-    ),
-    #[diagnostic(
-        code(Parser::Value::UnexpectedToken),
-        help("Enter a real number instead")
-    )]
-    ExpectedReal(
-        #[source_code] NamedSource<String>,
-        #[label("Here")] SourceSpan,
-    ),
-    #[diagnostic(code(Parser::Value::UnexpectedToken), help("Enter an id instead"))]
-    ExpectedId(
-        #[source_code] NamedSource<String>,
-        #[label("Here")] SourceSpan,
-    ),
-    #[diagnostic(code(Parser::Value::UnexpectedToken), help("Enter an ' instead"))]
-    ExpectedSingleQ(
-        #[source_code] NamedSource<String>,
-        #[label("Here")] SourceSpan,
-    ),
-    #[diagnostic(transparent, code(Parser::Value::UnexpectedEOF))]
-    Eof(#[from] UnexpectedEOF),
+pub trait Parser<'parser> {
+    type Output;
+    type Perr: Error + Diagnostic;
+
+    fn parse(
+        &self,
+        input: &'parser mut Lexer<'parser>,
+    ) -> ParserR<'parser, Self::Output, Self::Perr>;
+
+    fn pair<P2>(
+        &'parser self,
+        p2: P2,
+    ) -> impl Parser<'parser, Output = (Self::Output, P2::Output), Perr = Either<Self::Perr, P2::Perr>>
+    where
+        P2: Parser<'parser>,
+    {
+        move |input| match self.parse(input) {
+            Ok((output1, rest1)) => match p2.parse(rest1) {
+                Ok((output2, rest2)) => Ok(((output1, output2), rest2)),
+                Err(err) => Err(Either::Right(err)),
+            },
+            Err(err) => Err(Either::Left(err)),
+        }
+    }
+
+    fn expect(
+        &'parser self,
+        kind: Self::Output,
+    ) -> impl Parser<'parser, Output = Self::Output, Perr = Either<Self::Perr, Expected<Self::Output>>>
+    where
+        Self::Output: PartialEq + std::fmt::Debug,
+    {
+        move |input: &'parser mut Lexer<'parser>| {
+            let cloned = input.clone();
+            match self.parse(input) {
+                Ok((output, rest)) => {
+                    if output == kind {
+                        Ok((output, rest))
+                    } else {
+                        Err(Either::Right(Expected::new(
+                            output,
+                            rest.name,
+                            rest.src,
+                            cloned.offset..rest.offset,
+                        )))
+                    }
+                }
+                Err(err) => Err(Either::Left(err)),
+            }
+        }
+    }
+}
+
+impl<'parser, F, O, E: Error + Diagnostic> Parser<'parser> for F
+where
+    F: Fn(&'parser mut Lexer<'parser>) -> ParserR<'parser, O, E>,
+{
+    type Output = O;
+    type Perr = E;
+    fn parse(
+        &self,
+        input: &'parser mut Lexer<'parser>,
+    ) -> ParserR<'parser, Self::Output, Self::Perr> {
+        self(input)
+    }
 }
