1cb5095e704b553d7eea19c22db480127fabfe33
mrandr1an <krackedissad@gmail.com>
2024-10-09 12:57:34 +0300

    refactor(parser): rewrite parser module
    Use dynamic dispatch instead of static types for error handling.
    Remove Either error type.
    
    Seperate code segments, cleaner solution.

--- src/frontend/parsers/parser.rs
+++ src/frontend/parsers/parser.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, fmt::Debug, ops::Range};
+use std::ops::Range;
 
 use miette::{Diagnostic, NamedSource, SourceSpan};
 use thiserror::Error;
@@ -12,199 +12,286 @@ use crate::frontend::lexers::{
 pub type ParserR<'parser, Output, Error> =
     miette::Result<(Output, &'parser mut Lexer<'parser>), Error>;
 
-/* Parser Error Types */
-#[derive(Error, Diagnostic, Debug)]
-pub enum Either<E1: Error + Diagnostic, E2: Error + Diagnostic> {
-    #[error(transparent)]
-    #[diagnostic(transparent)]
-    Left(E1),
-    #[error(transparent)]
-    #[diagnostic(transparent)]
-    Right(E2),
-}
-
-impl<E1: Error + Diagnostic, E2: Error + Diagnostic> Either<E1, E2> {
-    pub fn lmap<F, O>(self, map_fn: F) -> Result<O, E2>
-    where
-        F: Fn(E1) -> O,
-    {
-        match self {
-            Self::Left(left) => Ok(map_fn(left)),
-            Self::Right(right) => Err(right),
-        }
-    }
-
-    pub fn rmap<F, O>(self, map_fn: F) -> Result<O, E1>
-    where
-        F: Fn(E2) -> O,
-    {
-        match self {
-            Self::Right(right) => Ok(map_fn(right)),
-            Self::Left(left) => Err(left),
-        }
-    }
-}
-
-#[derive(Error, Diagnostic, Debug)]
-#[error("EOF Error")]
-pub struct UnexpectedEOF {
-    #[source_code]
-    src: NamedSource<String>,
-    #[label("Here")]
-    bad_bit: SourceSpan,
-}
-
-impl UnexpectedEOF {
-    pub fn new(name: &str, src: &str, range: Range<usize>) -> Self {
-        Self {
-            src: NamedSource::new(name, src.to_string()),
-            bad_bit: range.into(),
-        }
-    }
-}
-
-#[derive(Error, Diagnostic, Debug)]
-#[error("Parsing Error")]
-#[diagnostic(code(parser::expected), severity(Error))]
-pub struct Expected<O: std::fmt::Debug> {
-    expected: O,
-    #[source_code]
-    src: NamedSource<String>,
-    #[label("Here")]
-    bad_bit: SourceSpan,
-}
-
-impl<O: std::fmt::Debug> Expected<O> {
-    pub fn new(expected: O, name: &str, src: &str, range: Range<usize>) -> Self {
-        Self {
-            expected,
-            src: NamedSource::new(name, src.to_string()),
-            bad_bit: range.into(),
-        }
-    }
-}
-
+/* General Parser Trait */
 pub trait Parser<'parser> {
     type Output;
-    type Perr: Error + Diagnostic;
-
     fn parse(
         &self,
         input: &'parser mut Lexer<'parser>,
-    ) -> ParserR<'parser, Self::Output, Self::Perr>;
+    ) -> ParserR<'parser, Self::Output, ParserError>;
 
-    fn pair<P2>(
-        &'parser self,
-        p2: P2,
-    ) -> impl Parser<'parser, Output = (Self::Output, P2::Output), Perr = Either<Self::Perr, P2::Perr>>
-    where
-        P2: Parser<'parser>,
-    {
-        move |input| match self.parse(input) {
-            Ok((output1, rest1)) => match p2.parse(rest1) {
-                Ok((output2, rest2)) => Ok(((output1, output2), rest2)),
-                Err(err) => Err(Either::Right(err)),
-            },
-            Err(err) => Err(Either::Left(err)),
-        }
-    }
-
-    fn map<F, O, P>(&self, map_fn: F) -> impl Parser<'parser, Output = O, Perr = Self::Perr>
+    fn map<F, O>(&self, map_fn: F) -> impl Parser<'parser, Output = O>
     where
         F: Fn(Self::Output) -> O,
     {
         move |input| match self.parse(input) {
-            Ok((output, rest)) => {
-                Ok::<(O, &mut Lexer<'parser>), Self::Perr>((map_fn(output), rest))
-            }
+            Ok((output, rest)) => Ok((map_fn(output), rest)),
+            Err(err) => Err(err),
+        }
+    }
+
+    fn pair<P>(&self, p: P) -> impl Parser<'parser, Output = (Self::Output, P::Output)>
+    where
+        P: Parser<'parser>,
+    {
+        move |input| match self.parse(input) {
+            Ok((output1, rest)) => match p.parse(rest) {
+                Ok((output2, rest)) => Ok(((output1, output2), rest)),
+                Err(err) => Err(err),
+            },
             Err(err) => Err(err),
         }
     }
 }
 
-/* Generic parser implementations */
+/* Parser Errors */
+type ParserError = Box<dyn Diagnostic + Send + Sync>;
 
-impl<'parser, F, O, E: Error + Diagnostic> Parser<'parser> for F
+#[derive(Error, Diagnostic, Debug)]
+#[error("Parser 'Expected' Error")]
+#[diagnostic(help("Expected {:#?}", self.expected))]
+pub struct Expected {
+    #[source_code]
+    src: NamedSource<String>,
+    #[label]
+    bad_bit: SourceSpan,
+    expected: TokenKind,
+}
+
+impl Expected {
+    pub fn new(name: &str, src: &str, range: Range<usize>, expected: TokenKind) -> Self {
+        Self {
+            src: NamedSource::new(name, src.to_string()),
+            bad_bit: range.into(),
+            expected,
+        }
+    }
+}
+
+#[derive(Error, Diagnostic, Debug)]
+#[error("Parser 'EOF' Error")]
+#[diagnostic(help("Expected {:#?} but got EOF (end of file)", self.expected))]
+pub struct UnexpectedEOF {
+    #[source_code]
+    src: NamedSource<String>,
+    #[label]
+    bad_bit: SourceSpan,
+    expected: TokenKind,
+}
+
+impl UnexpectedEOF {
+    pub fn new(name: &str, src: &str, range: Range<usize>, expected: TokenKind) -> Self {
+        Self {
+            src: NamedSource::new(name, src.to_string()),
+            bad_bit: range.into(),
+            expected,
+        }
+    }
+}
+
+/* Parser Implementors */
+impl<'parser, F, O> Parser<'parser> for F
 where
-    F: Fn(&'parser mut Lexer<'parser>) -> ParserR<'parser, O, E>,
+    F: Fn(&'parser mut Lexer<'parser>) -> ParserR<'parser, O, ParserError>,
 {
     type Output = O;
-    type Perr = E;
-    fn parse(
-        &self,
-        input: &'parser mut Lexer<'parser>,
-    ) -> ParserR<'parser, Self::Output, Self::Perr> {
+    fn parse(&self, input: &'parser mut Lexer<'parser>) -> ParserR<'parser, O, ParserError> {
         self(input)
     }
 }
 
 /* Primitive Parsers */
-
-pub fn expect<'parser>(
-    kind: TokenKind,
-) -> impl Parser<'parser, Output = TokenKind, Perr = Either<UnexpectedEOF, Expected<TokenKind>>> {
-    move |input: &'parser mut Lexer<'parser>| match input.next() {
+fn expect<'parser>(kind: TokenKind) -> impl Parser<'parser, Output = Token<'parser>> {
+    move |input: &'parser mut Lexer<'parser>| match input.peek() {
         Some(token) => {
-            if token.kind == kind {
-                Ok((token.kind, input))
+            if token.kind == kind.clone() {
+                input.next();
+                Ok((token, input))
             } else {
-                Err(Either::Right(Expected::new(
-                    token.kind,
+                Err(Box::new(Expected::new(
                     input.name,
                     input.src,
                     token.lexeme.range,
-                )))
+                    kind.clone(),
+                )) as ParserError)
             }
         }
-        None => Err(Either::Left(UnexpectedEOF::new(
+        None => Err(Box::new(UnexpectedEOF::new(
             input.name,
             input.src,
             input.offset..input.offset / 2,
-        ))),
+            kind.clone(),
+        )) as ParserError),
     }
 }
 
-/* Parser Blankets*/
-
-pub trait TokenParser<'parser>: Parser<'parser, Output = TokenKind> {
-    fn then_expect(
-        &self,
-        kind: TokenKind,
-    ) -> impl Parser<
-        'parser,
-        Output = Token<'parser>,
-        Perr = Either<Self::Perr, Either<UnexpectedEOF, Expected<TokenKind>>>,
-    > {
+/* Parser Blankets and Extension traits */
+pub trait TokenParser<'parser>: Parser<'parser> {
+    fn then_expect_ignore(&self, kind: TokenKind) -> impl Parser<'parser, Output = Self::Output> {
         move |input: &'parser mut Lexer<'parser>| {
-            let input = match self.parse(input) {
-                Ok((_, rest)) => rest,
-                Err(err) => return Err(Either::Left(err)),
-            };
-
-            match input.next() {
-                Some(token) => {
-                    if token.kind == kind {
-                        Ok((token, input))
-                    } else {
-                        Err(Either::Right(Either::Right(Expected::new(
-                            token.kind,
-                            input.name,
-                            input.src,
-                            token.lexeme.range,
-                        ))))
+            self.parse(input)
+                .map_or_else(Err, |(output, rest)| match rest.peek() {
+                    Some(token) => {
+                        if token.kind == kind.clone() {
+                            rest.next();
+                            Ok((output, rest))
+                        } else {
+                            Err(Box::new(Expected::new(
+                                rest.name,
+                                rest.src,
+                                rest.offset..rest.offset / 2,
+                                kind.clone(),
+                            )) as ParserError)
+                        }
                     }
-                }
-                None => Err(Either::Right(Either::Left(UnexpectedEOF::new(
-                    input.name,
-                    input.src,
-                    input.offset..input.offset / 2,
-                )))),
-            }
+                    None => Err(Box::new(UnexpectedEOF::new(
+                        rest.name,
+                        rest.src,
+                        rest.offset..rest.offset / 2,
+                        kind.clone(),
+                    )) as ParserError),
+                })
         }
     }
 }
 
-impl<'parser, E: Error + Diagnostic, P> TokenParser<'parser> for P where
-    P: Parser<'parser, Output = TokenKind, Perr = E>
+impl<'parser, P: Parser<'parser>> TokenParser<'parser> for P {}
+
+pub trait Paired<'parser, Left, Right>: Parser<'parser, Output = (Left, Right)> {
+    fn left(&self) -> impl Parser<'parser, Output = Left> {
+        self.map(|(left, _)| left)
+    }
+
+    fn right(&self) -> impl Parser<'parser, Output = Right> {
+        self.map(|(_, right)| right)
+    }
+}
+
+impl<'parser, Left, Right, P: Parser<'parser, Output = (Left, Right)>> Paired<'parser, Left, Right>
+    for P
 {
+}
+
+/*Parser Lib Tests*/
+#[cfg(test)]
+mod test {
+    use std::ops::Range;
+
+    use miette::{Diagnostic, NamedSource, SourceSpan};
+    use thiserror::Error;
+
+    use crate::frontend::lexers::{lexer::Lexer, token::TokenKind};
+
+    use super::{expect, Paired, Parser, ParserError, ParserR, TokenParser};
+
+    fn some_parser<'parser>(
+        input: &'parser mut Lexer<'parser>,
+    ) -> ParserR<'parser, String, ParserError> {
+        #[derive(Error, Diagnostic, Debug)]
+        #[error("whhooops!")]
+        #[diagnostic(help("Skill issue"))]
+        pub struct SomeParserError {
+            #[source_code]
+            src: NamedSource<String>,
+            #[label("This bit")]
+            bad_bit: SourceSpan,
+        }
+
+        impl SomeParserError {
+            fn new(name: &str, src: &str, range: Range<usize>) -> Self {
+                Self {
+                    src: NamedSource::new(name, src.to_string()),
+                    bad_bit: range.into(),
+                }
+            }
+        }
+
+        match input.next() {
+            Some(token) => match token.kind {
+                TokenKind::Unknown => Ok((String::from(token.lexeme.slice), input)),
+                _ => Err(Box::new(SomeParserError::new(
+                    input.name,
+                    input.src,
+                    token.lexeme.range,
+                ))),
+            },
+            None => panic!("This should never happen"),
+        }
+    }
+
+    #[test]
+    fn some_parser_test() -> miette::Result<()> {
+        let mut lexer = Lexer::new("main.eur", "Hello world");
+        match some_parser.parse(&mut lexer) {
+            Ok((string, _rest)) => assert_eq!("Hello".to_string(), string),
+            Err(err) => return Err(miette::Report::new_boxed(err)),
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn map_some_parser() -> miette::Result<()> {
+        let mut lexer = Lexer::new("main.eur", "Hello world");
+        match some_parser.map(|output| output.len()).parse(&mut lexer) {
+            Ok((len, _rest)) => assert_eq!(5, len),
+            Err(err) => return Err(miette::Report::new_boxed(err)),
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn pair_parser() -> miette::Result<()> {
+        let mut lexer = Lexer::new("main.eur", "Hello world");
+        match some_parser
+            .map(|output| output.len())
+            .pair(some_parser)
+            .parse(&mut lexer)
+        {
+            Ok(((len, string), _rest)) => assert_eq!((5, "world".to_string()), (len, string)),
+            Err(err) => return Err(miette::Report::new_boxed(err)),
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn then_ignore_parser() -> miette::Result<()> {
+        let mut lexer = Lexer::new("main.eur", "Hello friend ignored_token");
+        match some_parser
+            .map(|output| output.len())
+            .pair(some_parser)
+            .then_expect_ignore(TokenKind::Unknown)
+            .parse(&mut lexer)
+        {
+            Ok(((len, string), _rest)) => assert_eq!((5, "friend".to_string()), (len, string)),
+            Err(err) => return Err(miette::Report::new_boxed(err)),
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn paired_parser() -> miette::Result<()> {
+        let mut lexer = Lexer::new("main.eur", "Hello friend ignored_token");
+        match some_parser
+            .map(|output| output.len())
+            .pair(some_parser)
+            .then_expect_ignore(TokenKind::Unknown)
+            .left()
+            .parse(&mut lexer)
+        {
+            Ok((len, _rest)) => assert_eq!(5, len),
+            Err(err) => return Err(miette::Report::new_boxed(err)),
+        }
+        Ok(())
+    }
+
+    #[test]
+    #[should_panic]
+    fn expect_parser() {
+        let mut lexer = Lexer::new("main.eur", "5 friend ignored_token");
+        let res = match expect(TokenKind::Unknown).parse(&mut lexer) {
+            Ok((token, _)) => Ok(println!("{:#?}", token)),
+            Err(err) => Err(miette::Report::new_boxed(err)),
+        };
+        res.unwrap()
+    }
 }
